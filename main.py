36bdbc7994e24d57a1832d036a8b15c95700316b
nice2minjoo <102221871+nice2minjoo@users.noreply.github.com>
2023-12-20 19:10:25 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import re
+import datetime as dt
+import time
+# to add days or years
+from dateutil.relativedelta import relativedelta
+import gspread
+import pandas as pd
+
+credentials = st.secrets["service_account"]
+sa = gspread.service_account("credentials")
+sh = sa.open("회비")
+worksheet_list = sh.worksheets() # The total number of worksheets
+
+st.write(len(worksheet_list))
